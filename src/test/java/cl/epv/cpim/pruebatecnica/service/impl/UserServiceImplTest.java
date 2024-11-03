337898a40b8109ccd31390acc5afbc56c7d15f93
Eduardo Pino <eduardo.pino.vallejos@gmail.com>
2024-11-03 19:48:19 -0300

    Se agregan tests unitarios

---
+++ src/test/java/cl/epv/cpim/pruebatecnica/service/impl/UserServiceImplTest.java
@@ -0,0 +1,32 @@
+package cl.epv.cpim.pruebatecnica.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void newUser() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void testGetUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
